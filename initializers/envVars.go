d2047c00fad1275078a0808c0a2e0bc0ae8d882a
Chong Tze Zhao <47619363+chongtzezhao@users.noreply.github.com>
2024-02-10 03:43:03 +0800

    switched environment variables to OS

--- initializers/envVars.go
+++ initializers/envVars.go
@@ -2,6 +2,7 @@ package initializers
 
 import (
 	"log"
+	"os"
 
 	"github.com/joho/godotenv"
 )
@@ -10,6 +11,6 @@ func LoadEnvVariables() {
 	err := godotenv.Load()
 
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Print("Error loading .env file")
 	}
 }
